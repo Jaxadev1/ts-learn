9ef5212cf48570e215fc0400c1db50db62da19b7
Jahongir Aminov <jahongiraminov@192.168.1.2>
2024-12-13 22:09:18 +0500

    commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Greet } from "./Greet"
+
+function App() {
+
+  return (
+    <>
+      <Greet/>      
+    </>
+  )
+}
+
+export default App
---
+++ src/Greet.tsx
@@ -0,0 +1,7 @@
+export const Greet: React.FC = () => {
+  return(
+    <div>
+      <h2>Welcome Nigga , you have 10 unread messages</h2>
+    </div>
+  )
+} 
